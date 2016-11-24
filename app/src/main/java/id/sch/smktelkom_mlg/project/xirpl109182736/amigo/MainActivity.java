17a21f0e3c31f18a6558e8740eb6975283649307
M. Nur Arifin <arifinmumtazz@gmail.com>
2016-11-24 14:15:50 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl109182736/amigo/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl109182736.amigo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
